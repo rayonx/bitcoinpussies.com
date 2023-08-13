2743fc140578689c92d096ed3a4514efccb58996
Ray <hello@ray>
2023-08-13 14:31:16 +0800

    add daisy themes, scaffolding cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,10 @@ module.exports = {
 
     // daisyUI config (optional - here are the default values)
     daisyui: {
-        themes: false, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
-        darkTheme: "dark", // name of one of the included themes for dark mode
+        themes: [
+            'dracula'
+        ], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+        darkTheme: "dracula", // name of one of the included themes for dark mode
         base: true, // applies background color and foreground color for root element by default
         styled: true, // include daisyUI colors and design decisions for all components
         utils: true, // adds responsive and modifier utility classes
